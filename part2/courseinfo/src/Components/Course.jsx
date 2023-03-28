53152fdcbcdcb4b9ecf7bbd7d85e314774fde4e8
Xu Che <xuche1998@gmail.com>
2023-03-28 13:20:31 +0800

    ex2.1 edit

--- part2/courseinfo/src/Components/Course.jsx
+++ part2/courseinfo/src/Components/Course.jsx
@@ -14,15 +14,6 @@ const Part = ({ part, exercises }) => {
     )
 }
 
-const Total = ({ exercise1, exercise2, exercise3 }) => {
-
-    return (
-        <p>
-            Number of exercises {exercise1 + exercise2 + exercise3}
-        </p>
-    )
-}
-
 const Course = ({ course }) => {
     return (
         <>
