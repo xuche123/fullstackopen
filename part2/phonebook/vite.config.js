c58724e27bd07c2379c46cc96aaff4737e846cdd
Xu Che <xuche1998@gmail.com>
2023-03-31 01:25:04 +0800

    ex3.11 fix proxy

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api/people': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+        rewriteHost: true,
+      },
+    },
+  },
   plugins: [react()],
 })
