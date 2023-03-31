f029c63575dcd0ad4e28f196045c13ffc97d0855
Xu Che <xuche1998@gmail.com>
2023-03-31 18:35:30 +0800

    ex3.15-3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -22,31 +22,41 @@ app.use(morgan(function (tokens, req, res) {
 }))
 
 app.get('/', (req, res) => {
-    Person.countDocuments({}).then(count => {
-        res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
-    })
+    Person.countDocuments({})
+        .then(count => {
+            res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
+        })
 })
 
 app.get('/api/people', (req, res) => {
-    Person.find({}).then(people => {
-        res.json(people)
-    })
+    Person.find({})
+        .then(people => {
+            res.json(people)
+        })
 })
 
-app.get('/api/people/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person)
-    })
+app.get('/api/people/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person)
+            } else {
+                res.status(404).end()
+            }
+        }).catch(error => {
+            next(error)
+        })
 })
 
-app.delete('/api/people/:id', (req, res) => {
-    const id = Number(req.params.id)
-    people = people.filter(person => person.id !== id)
-
-    res.status(204).end()
+app.delete('/api/people/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/people', (req, res) => { 
+app.post('/api/people', (req, res) => {
     const body = req.body
 
     if (!body.name) {
@@ -66,12 +76,56 @@ app.post('/api/people', (req, res) => {
         number: body.number
     })
 
-    person
-        .save()
+    person.save()
         .then(savedPerson => {
             res.json(savedPerson)
         })
 })
+
+app.put('/api/people/:id', (req, res, next) => {
+    const body = req.body
+
+    if (!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(
+        req.params.id,
+        person,
+        { new: true, runValidators: true, context: 'query' })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message)
+
+    if (err.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(err)
+}
+
+app.use(errorHandler)
+// this has to be the last loaded middleware.
 
 const PORT = process.env.PORT || 3001
 
