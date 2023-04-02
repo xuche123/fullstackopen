9240cb3bde3dc46c87a84a3d5ec1f881dedeb53b
Xu Che <xuche1998@gmail.com>
2023-04-02 18:14:01 +0800

    ex4.2 lint

--- part4/bloglist/models/blogs.js
+++ part4/bloglist/models/blogs.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+	title: String,
+	author: String,
+	url: String,
+	likes: Number
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
