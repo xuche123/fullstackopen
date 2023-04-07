f6cfcebb5200aa9f55599a15e9cb56576ba50815
Xu Che <xuche1998@gmail.com>
2023-04-07 19:41:04 +0800

    ex7.3 refactor

---
+++ part7/routed-anecdotes/src/components/Notification.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Notification = ({ notification }) => {
+    const style = {
+        border: 'solid',
+        padding: 10,
+        borderWidth: 1
+    }
+
+    if (notification === '') {
+        return null
+    }
+
+    return (
+        <div style={style}>
+            {notification}
+        </div>
+    )
+}
+
+export default Notification
